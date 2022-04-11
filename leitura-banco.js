e8749b9b23d335f4e1089ceddf5014437ffac10b
Alminhaa <eualvesgabriel@gmail.com>
2022-04-11 14:47:11 -0300

    alterando banco de dados

--- leitura-banco.js
+++ leitura-banco.js
@@ -18,8 +18,6 @@ const objs =
     // enviando para a API
 const jsonData = JSON.stringify(objs)
 
-console.log(jsonData)
-console.log(typeof jsonData)
 
     //converter JSON para objeto
 
@@ -34,25 +32,77 @@ console.log(typeof objData)
         console.log(id)
     })
 
-    //DAQUI PARA BAIXO É O PROBLEMA, NÃO CONSIGO ALTERAR AS INFORMAÇÕES DO BANCO DE DADOS
+    //alterando os dados do banco de dados
 
-    //Utilizei o updateDoc que em um video, mas não consigo achar a referencia do banco de dados para ele mapear
-    // e fazer a alteração
+    console.log(objData[0].id);
+    objData[0].name = 'Conjunto de Panelas antiaderentes com 05 Peças Paris';
+    console.log(objData[0].name);
+    console.log(objData[0].price);
+    console.log(typeof objData[0].price);
+    console.log(objData[0].category);
+    console.log(objData[0]);
 
-  
+    console.log(objData[1].id);
+    objData[1].name = 'Lava & Seca 10,2 Kg Samsung Eco Bubble Branca com 09 Programas de Lavagem';
+    console.log(objData[1].name);
+    console.log(objData[1].price);
+    console.log(objData[1].category);
+    console.log(objData[1]);
 
-    updateDoc(objData, 
-        [ 
-        { id: 5677240, name: "Conjunto de Panelas æntiaderentes com 05 Peças Paris", quantity: 21, price: 192.84, Category: "Panelas" }, 
-        { id: 9628920, name: "Lava & Seca 10,2 Kg Samsung Eco Bubble Brança com 09 Programas de Lavagem", quantity: 57, price: 3719.70, category: "Eletrodomésticos" }, 
-        { id: 1316334, name: "Refrigerador Bottom Freezer Electrolux de 02 Portas Frost Free com 598 Litros", quantity: 12, price: 3880.23, category: "Eletrodomésticos" }, 
-        { id: 6502394, name: "Fogão de Piso Electrolux de 04 Bocas, Mesa de Vidro Prata", quantity: 37, price: 1419, category: "Eletrodomésticos" }, 
-        { id: 9576720, name: "Forno Micro-ondas Panasonic com capacidade de 21 Litros Branco", quantity: 13, price: 358.77, category: "Eletrodomésticos" }, 
-        { id: 8875900, name: "Smart TV 4K Sony LED 65” 4K X-Reality Pro, UpScalling, Motionflow XR 240 e Wi-Fi", quantity: 0, price: 5799.42, category: "Eletrônicos" }, 
-        { id: 9746439, name: "Home Theater LG com Blu-ray 3D, 5.1 canais e 1000W", quantity: 80, price: 2199, category: "Eletrônicos" }, 
-        { id: 2162952, name: "Kit Gamer acer - NoteBook + Headset + Mouse", price: 25599.00, category: "Eletrônicos" }, 
-        { id: 3500957, name: "Monitor 29 LG FHD Ultrawide com 1000:1 de contraste", quantity: 18, price: 1559.40, category: "Eletrônicos" }, 
-        { id: 1911864, name: "Mouse Gamer Predator Cestus 510 Fox Preto", price: 699, category: "Acessórios" } ] ) 
-        
-        .then(() => console.log("Documento Atualizado")) 
-        .catch(console.log)
+    console.log(objData[2].id);
+    objData[2].name = 'Refrigerador Bottom Freezer Electrolux de 02 Portas Frost Free com 598 Litros';
+    console.log(objData[2].name);
+    console.log(objData[2].price);
+    console.log(objData[2].category);
+    console.log(objData[2]);
+
+    console.log(objData[3].id);
+    objData[3].name = 'Fogão de Piso Electrolux de 04 Bocas, Mesa de Vidro Prata';
+    console.log(objData[3].name);
+    console.log(objData[3].price);
+    console.log(objData[3].category);
+    console.log(objData[3]);
+
+    console.log(objData[4].id);
+    objData[4].name = 'Forno Micro-ondas Panasonic com capacidade de 21 Litros Branco';
+    console.log(objData[4].name);
+    console.log(objData[4].price);
+    console.log(objData[4].category);
+    console.log(objData[4]);
+
+    console.log(objData[5].id);
+    objData[5].name = 'Smart TV 4K Sony LED 65” 4K X-Reality Pro, UpScalling, Motionflow XR 240 e Wi-Fi';
+    console.log(objData[5].name);
+    console.log(objData[5].price);
+    console.log(objData[5].category);
+    console.log(objData[5]);
+
+    console.log(objData[6].id);
+    objData[6].name = 'Home Theater LG com Blu-ray 3D, 5.1 canais e 1000W';
+    console.log(objData[6].name);
+    console.log(objData[6].price);
+    console.log(objData[6].category);
+    console.log(objData[6]);
+
+    console.log(objData[7].id);
+    objData[7].name = 'Kit Gamer acer - NoteBook + Headset + Mouse';
+    console.log(objData[7].name);
+    console.log(objData[7].price);
+    console.log(objData[7].category);
+    console.log(objData[7]);
+
+    console.log(objData[8].id);
+    objData[8].name = 'Monitor 29 LG FHD Ultrawide com 1000:1 de contraste';
+    console.log(objData[8].name);
+    console.log(objData[8].price);
+    console.log(objData[8].category);
+    console.log(objData[8]);
+
+    console.log(objData[9].id);
+    objData[9].name = 'Mouse Gamer Predator Cestus 510 Fox Preto';
+    console.log(objData[9].name);
+    console.log(objData[9].price);
+    console.log(objData[9].category);
+    console.log(objData[9]);
+
+    console.log(objData);
